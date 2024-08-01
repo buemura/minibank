608a9bdedff52bf15183368ec0a584ae14bed8f6
buemura <bruno.uemura1@gmail.com>
2024-08-01 16:26:47 -0300

    feat: adds get account usecase

--- apps/svc-account/internal/core/repository/account_repository.go
+++ apps/svc-account/internal/core/repository/account_repository.go
@@ -1,4 +1,7 @@
 package repository
 
+import "github.com/buemura/minibank/svc-account/internal/core/entity"
+
 type AccountRepository interface {
+	GetAccountById(id string) (*entity.Account, error)
 }
